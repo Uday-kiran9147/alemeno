5f17998d815f9b65a2b41260ce4e4eec1e913b70
Uday-kiran9147 <udaykiran9147@gmail.com>
2023-09-22 09:58:07 +0530

    font added

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          fontFamily: 'Andika'),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
